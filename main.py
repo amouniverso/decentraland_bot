7816f71c6459189439451f559473eec8454317c5
Filipp <amouniverso@yandex.ru>
2022-11-21 22:15:20 +0300

    moves

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ sys.path.append('yolov7-main')
 import detect as yolov7
 
 
-def moveMouse(x, y, duration):
+def rel_mouse_event(x, y):
+    win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0)
+
+
+def move_mouse(x, y, duration):
     steps = 20
     for i in range(0, steps):
-        win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, int(x / steps), int(y / steps), 0, 0)
+        rel_mouse_event(int(x / steps), int(y / steps))
         sleep(duration / steps)
 
 def moveChar():
@@ -23,37 +27,73 @@ def moveChar():
     pyautogui.keyDown('s')
     sleep(2)
     pyautogui.keyUp('s')
-    moveMouse(200, 0, 1)
+    move_mouse(200, 0, 1)
     sleep(1)
-    moveMouse(200, 0, 1)
+    move_mouse(200, 0, 1)
     sleep(1)
-    moveMouse(200, 0, 1)
+    move_mouse(200, 0, 1)
     sleep(1)
-    moveMouse(200, 0, 1)
+    move_mouse(200, 0, 1)
     sleep(1)
     print('char: rest.')
 
-
-wincap = WindowCapture('Decentraland')
 loop_time = time()
 print('Started.')
+detector = yolov7.Detector('yolov7-main/yolov7_custom300epochs.pt', 0.45, 640, True, True)
+wincap = WindowCapture('Decentraland')
 
+rest_time = time()
 while True:
-    img = yolov7.detect('yolov7-main/yolov7_custom300epochs.pt', 0.1, 640, wincap.get_screenshot(), True, True)
-    # get an updated image of the game
-    #imS = cv.resize(wincap.get_screenshot(), (int(wincap.w / 1.5), int(wincap.h / 1.5)))
-    #cv.imshow('Computer Vision', img)
+    print('rest time', time() - rest_time)
+    windowCenterY = wincap.h / 2
+    windowCenterX = wincap.w / 2
+    img, det = detector.detect(wincap.get_screenshot())
+    pyautogui.keyUp('w')
+    pyautogui.keyUp('s')
+
+    dBoxCenterX = None
+    dBoxCenterY = None
+    c1 = None
+    c2 = None
+    if len(det):
+        for *xyxy, conf, cls in reversed(det):
+            c1, c2 = (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3]))
+            print('Detection box: ', c1, c2)
+            c1x, c1y = c1
+            c2x, c2y = c2
+            dBoxCenterX = c1x + ((c2x - c1x) / 2)
+            dBoxCenterY = c1y + ((c2y - c1y) / 2)
+            print('Detection box center, xy: ', dBoxCenterX, dBoxCenterY)
+    imS = cv.resize(img, (int(wincap.w / 1.5), int(wincap.h / 1.5)))
+    cv.imshow('Computer Vision', imS)
+    # cv.waitKey(1)  # 1 millisecond
+
+    if dBoxCenterY is not None and dBoxCenterX is not None:
+        rest_time = time()
+        diffX = windowCenterX - dBoxCenterX
+        diffY = windowCenterY - dBoxCenterY
+        print('DIFF, xy:', int(diffX), int(diffY))
+        print('c2[0] - c1[0]', c2[0] - c1[0])
+        move_mouse(int(-diffX), int(-diffY + 220), 0.1)
+        if c2[0] - c1[0] < 600:
+            pyautogui.keyDown('w')
+        else:
+            pyautogui.keyUp('w')
+            pyautogui.click()
+            pyautogui.keyDown('s')
+            # move_mouse(2500, 0, 0.5)
+    else:
+        if time() - rest_time > 3:
+            print('RESET')
+            pyautogui.click()
+            move_mouse(1000, -100, 0.1)
+            rest_time = time()
 
     # debug the loop rate
     print('FPS {}'.format(int(1 / (time() - loop_time))))
     loop_time = time()
-    # press 'q' with the output window focused to exit.
-    # waits 1 ms every loop to process key presses
-    input("Press Enter to continue...")
     if cv.waitKey(1) == ord('q'):
        cv.destroyAllWindows()
        break
-    #sleep(3)
-    break
-
+    # sleep(1)
 print('Done.')
--- yolov7-main/detect.py
+++ yolov7-main/detect.py
@@ -15,84 +15,85 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
-class Yolov7:
-    def __init__(self, weights, conf_thres, imgsz, source, view_img, trace, save_img=False, save_txt=False, device=''):
-        img_size = imgsz
-        augment = False
-        agnostic_nms = False
-        classes = None
-        iou_thres = 0.45
+class Detector:
+    def __init__(self, weights, conf_thres, imgsz, view_img, trace, device=''):
+        self.conf_thres = conf_thres
+        self.view_img = view_img
+        self.img_size = imgsz
+        self.augment = False
+        self.agnostic_nms = False
+        self.classes = None
+        self.iou_thres = 0.45
 
         # Initialize
         set_logging()
-        device = select_device(device)
-        half = device.type != 'cpu'  # half precision only supported on CUDA
+        self.device = select_device(device)
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
 
         # Load model
-        model = attempt_load(weights, map_location=device)  # load FP32 model
-        stride = int(model.stride.max())  # model stride
-        imgsz = check_img_size(imgsz, s=stride)  # check img_size
+        self.model = attempt_load(weights, map_location=self.device)  # load FP32 model
+        self.stride = int(self.model.stride.max())  # model stride
+        self.imgsz = check_img_size(imgsz, s=self.stride)  # check img_size
 
         if trace:
-            model = TracedModel(model, device, img_size)
+            self.model = TracedModel(self.model, self.device, self.img_size)
 
-        if half:
-            model.half()  # to FP16
+        if self.half:
+            self.model.half()  # to FP16
 
         # Second-stage classifier
-        classify = False
-        if classify:
-            modelc = load_classifier(name='resnet101', n=2)  # initialize
-            modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+        self.classify = False
+        if self.classify:
+            self.modelc = load_classifier(name='resnet101', n=2)  # initialize
+            self.modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=self.device)['model']).to(self.device).eval()
 
         # Get names and colors
-        names = model.module.names if hasattr(model, 'module') else model.names
-        colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
 
         # Run inference
-        if device.type != 'cpu':
-            model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-        old_img_w = old_img_h = imgsz
-        old_img_b = 1
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, imgsz, imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
 
-    def detect(self, weights, conf_thres, imgsz, source, view_img, trace, save_img=False, save_txt=False, device=''):
+    def detect(self, source):
         # Set Dataloader
-        dataset = LoadImages(source, img_size=imgsz, stride=stride)
+        dataset = LoadImages(source, img_size=self.imgsz, stride=self.stride)
+        old_img_w = old_img_h = self.imgsz
+        old_img_b = 1
 
         t0 = time.time()
         for path, img, im0s, vid_cap in dataset:
-            img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if self.half else img.float()  # uint8 to fp16/32
             img /= 255.0  # 0 - 255 to 0.0 - 1.0
             if img.ndimension() == 3:
                 img = img.unsqueeze(0)
 
             # Warmup
-            if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
+            if self.device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
                 old_img_b = img.shape[0]
                 old_img_h = img.shape[2]
                 old_img_w = img.shape[3]
                 for i in range(3):
-                    model(img, augment=augment)[0]
+                    self.model(img, augment=self.augment)[0]
 
             # Inference
             t1 = time_synchronized()
             with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-                pred = model(img, augment=augment)[0]
+                pred = self.model(img, augment=self.augment)[0]
             t2 = time_synchronized()
 
             # Apply NMS
-            pred = non_max_suppression(pred, conf_thres, iou_thres, classes=classes, agnostic=agnostic_nms)
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
             t3 = time_synchronized()
 
             # Apply Classifier
-            if classify:
-                pred = apply_classifier(pred, modelc, img, im0s)
+            if self.classify:
+                pred = apply_classifier(pred, self.modelc, img, im0s)
 
             # Process detections
             for i, det in enumerate(pred):  # detections per image
                 p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
-
                 if len(det):
                     # Rescale boxes from img_size to im0 size
                     det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
@@ -100,19 +101,21 @@ class Yolov7:
                     # Print results
                     for c in det[:, -1].unique():
                         n = (det[:, -1] == c).sum()  # detections per class
-                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                        s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                     # Write results
                     for *xyxy, conf, cls in reversed(det):
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        im0 = plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
+                        label = f'{self.names[int(cls)]} {conf:.2f}'
+                        im0 = plot_one_box(xyxy, im0, label=label, color=self.colors[int(cls)], line_thickness=4)
 
                 # Print time (inference + NMS)
                 print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
-                # Stream results
-                if view_img:
-                    cv2.imshow(str(p), im0)
-                    cv2.waitKey(1)  # 1 millisecond
+                return im0, det
+                # # Stream results
+                # if self.view_img:
+                #     cv2.imshow(str(p), im0)
+                #     cv2.waitKey(1)  # 1 millisecond
 
         print(f'Done. ({time.time() - t0:.3f}s)')
+
